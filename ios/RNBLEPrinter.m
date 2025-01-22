606f534232ff6956095eb5b19f2d2f4cd3235543
Johnsonzhangc <johnson.zhangc@gmail.com>
2025-01-22 14:03:35 +0700

    Update RNBLEPrinter.m

--- ios/RNBLEPrinter.m
+++ ios/RNBLEPrinter.m
@@ -216,7 +216,7 @@ RCT_EXPORT_METHOD(sendHex:(NSString *)hex
        newHeight = [nHeight floatValue];
    }
 
-   CGFloat paddingX = 0;
+   CGFloat paddingX = 250;
    if(nPaddingX != nil) {
        paddingX = [nPaddingX floatValue];
    }
