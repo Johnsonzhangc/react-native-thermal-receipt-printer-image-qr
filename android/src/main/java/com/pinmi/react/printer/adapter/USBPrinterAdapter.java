0c316edd786d1015ab23a9ead5b9e6fe1b3c3b7e
Johnsonzhangc <johnson.zhangc@gmail.com>
2025-02-04 22:34:29 +0700

    Update USBPrinterAdapter.java

--- android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterAdapter.java
+++ android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterAdapter.java
@@ -111,7 +111,7 @@ public class USBPrinterAdapter implements PrinterAdapter {
         //Resolve the Android SDK >= 34 cannot use FLAG_MUTABLE problem
         //this.mPermissionIndent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
         if (Build.VERSION.SDK_INT >= 34) {
-            this.mPermissionIndent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+            this.mPermissionIndent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT | PendingIntent.FLAG_UPDATE_CURRENT);
         } else {
             this.mPermissionIndent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
         }
@@ -122,7 +122,7 @@ public class USBPrinterAdapter implements PrinterAdapter {
         //Resove Android SDK >=34
         //mContext.registerReceiver(mUsbDeviceReceiver, filter);
         if (Build.VERSION.SDK_INT >= 34) {
-            mContext.registerReceiver(mUsbDeviceReceiver, filter, Context.RECEIVER_EXPORTED);
+            mContext.registerReceiver(mUsbDeviceReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
         } else {
             mContext.registerReceiver(mUsbDeviceReceiver, filter);
         }
